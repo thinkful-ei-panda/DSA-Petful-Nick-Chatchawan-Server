8809e001f9f1a2745e688fa11c2144ae1e2ae931
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-28 17:55:32 -0500

    removes cors

--- modules/app/app.js
+++ modules/app/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 app.use('/people', require('../people/people.router'));
 app.use('/pets', require('../pets/pets.router'));
